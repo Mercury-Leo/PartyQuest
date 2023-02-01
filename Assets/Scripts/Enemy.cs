ad52c9ab88722b0951733b399282ddf99b3e88c7
Mercury <>
2023-02-01 11:10:56 +0200

    Uploading old Party Quest POC project for archive

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float spawnRate = 0.1f;
+    [SerializeField] private float catchRate = 0.1f;
+    [SerializeField] private int attack = 0;
+    [SerializeField] private int defenece = 0;
+    [SerializeField] private int hp = 10;
+    [SerializeField] private AudioClip crySound;
+
+    private AudioSource audioSource;
+
+    public float SpawnRate { get => spawnRate; set => spawnRate = value; }
+    public float CatchRate { get => catchRate; set => catchRate = value; }
+    public int Attack { get => attack; set => attack = value; }
+    public int Defenece { get => defenece; set => defenece = value; }
+    public int Hp { get => hp; set => hp = value; }
+    public AudioClip CrySound { get => crySound; set => crySound = value; }
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        Assert.IsNotNull(audioSource);
+        Assert.IsNotNull(CrySound);
+    }
+
+    private void OnMouseDown()
+    {
+        PartyQuestSceneManager[] managers = FindObjectsOfType<PartyQuestSceneManager>();
+        audioSource.PlayOneShot(CrySound);
+        foreach(PartyQuestSceneManager partyQuestSceneManager in managers)
+        {
+            if (partyQuestSceneManager.gameObject.activeSelf)
+            {
+                partyQuestSceneManager.enemyTapped(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PlayerData
+{
+
+    private int exp;
+    private int req_exp;
+    private int levelBase;
+    private int level;
+    private List<GameObject> items;
+
+    public int Exp { get => exp; set => exp = value; }
+    public int RequiredExp { get => req_exp; set => req_exp = value; }
+    public int LevelBase { get => levelBase; set => levelBase = value; }
+    public int Level { get => level; set => level = value; }
+    public List<GameObject> Items { get => items; set => items = value; }
+
+    public PlayerData(Player player)
+    {
+        this.exp = player.Exp;
+        this.req_exp = player.Req_exp;
+        this.levelBase = player.LevelBase;
+        this.level = player.Level;
+        this.items = player.Items;
+        /*
+        foreach (GameObject itemsObj in player.Items)
+        {
+            Enemy enemy = itemsObj.GetComponent<Enemy>();//should be ITEMS ofcourse and not enemy
+            if(enemy != null)
+            {
+                EnemyData data = new EnemyData(enemy);
+                items.Add(data);
+            }
+        } */
+    }
+}
